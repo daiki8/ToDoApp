d10e902ece9873665c97071e441375b2d3523468
daiki8 <daiki1945815@gmail.com>
2018-09-18 15:50:54 +0000

    add task-creater

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,9 +1,20 @@
 class TasksController < ApplicationController
   def index
+    @task = Task.new
     @tasks = Task.all
   end
   
   def create
+    @task = Task.new(task_params)
+    if @task.save
+      redirect_to root_url
+    else
+      @tasks = Task.all # この対処法でいいのか？
+      render 'tasks/index'
+    end
+  end
+  
+  def edit
   end
   
   def update
@@ -11,4 +22,10 @@ class TasksController < ApplicationController
   
   def delete
   end
+  
+  private
+  
+    def task_params
+      params.require(:task).permit(:title)
+    end
 end
